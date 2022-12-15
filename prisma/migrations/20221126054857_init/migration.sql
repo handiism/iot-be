2a2a9e5db1e81c58396a1a66208d7596ff8908cd
handiism <mail.handira@gmail.com>
2022-12-15 09:02:08 +0700

    feat: migrate to mysql

--- prisma/migrations/20221126054857_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "Lamp" (
-    "id" SERIAL NOT NULL,
-    "status" BOOLEAN NOT NULL,
-    "time" TIMESTAMP(3) NOT NULL,
-
-    CONSTRAINT "Lamp_pkey" PRIMARY KEY ("id")
-);
