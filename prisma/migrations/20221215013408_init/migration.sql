a351c71c2743323ea0335c9a0cd6a86923dc6d40
handiism <mail.handira@gmail.com>
2022-12-19 12:55:40 +0700

    fix: ch from postgresql to sqlite

--- prisma/migrations/20221215013408_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE `lamp` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `status` BOOLEAN NOT NULL,
-    `time` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
-
-    PRIMARY KEY (`id`)
-) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
